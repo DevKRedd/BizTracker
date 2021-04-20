9b81af517d169815fbd574f4ec9c19dd4506e7a2
Khalin Redding <khalinredd@gmail.com>
2021-04-20 18:28:03 -0400

    MVP Stretch 70% Done

--- app/serializers/user_dashboard_serializer.rb
+++ app/serializers/user_dashboard_serializer.rb
@@ -1,4 +1,4 @@
 class UserDashboardSerializer < ActiveModel::Serializer
-  attributes :id, :dashboard_title, :chart_type, :quarter_number
+  attributes :id, :dashboard_title, :chart_type, :quarter_number, :year
   has_one :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
-      get '/login', to: 'users#token_authenticate'
+      # get '/login', to: 'users#token_authenticate'
       post '/login', to: 'auth#create'
       post '/signup', to: 'users#create'
       get '/profile', to: 'users#profile'
